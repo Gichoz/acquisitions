f28b20787e6733f151c870baa78228d4f5bcfc20
Kelvin <kelvingichohi50@gmail.com>
2026-08-28 06:57:22 +0300

    Add security middleware to enhance request validation and rate limiting

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+import securityMiddleware from './middleware/security.middleware.js';
 import authRoutes from './routes/auth.routes.js';
 
 const app = express();
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(securityMiddleware);
 
 app.use(
   morgan('combined', {
