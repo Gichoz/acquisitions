5a00cadecc5337da8228eca1517e7b17e26c653f
Kelvin <kelvingichohi50@gmail.com>
2026-08-30 14:37:14 +0300

    add .dockerignore and standardize single-quote formatting

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 
+dotenv.config();
 import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
